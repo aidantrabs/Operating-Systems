301b130dd3e11a75a9735a207ccd63d2ca7ff1bb
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-09-29 00:51:08 -0400

    Update file handler

--- A01/filecopy.c
+++ A01/filecopy.c
@@ -8,44 +8,46 @@ Email: trab5590@mylaurier.ca
 
 #include <stdio.h>
 #include <stdlib.h>
-
-#include <sys/types.h>
-#include <dirent.h>
-#include <sys/stat.h>
 #include <unistd.h>
+#include <fcntl.h>
 
-int main() {
+int main(int argc, char *argv[]) {
 
-     char input_file_name[100];
-     char output_file_name[100];
+     /* Declare variables */
+     int filedir_src, filedir_des, line;
+     char buf[BUFSIZ];
 
-     printf("Enter the name of the input file:\n");
-     scanf("%s", input_file_name);
-
-     printf("Enter the name of the output file:\n");
-     scanf("%s", output_file_name);
-
-     FILE *input_file = fopen(input_file_name, "r");
-     FILE *output_file = fopen(output_file_name, "w");
-
-     if (input_file == NULL) {
-          printf("Input file does not exist\n");
-          return 0;
+     /* Output error if user does not pass parameters */
+     if (argc != 3) {
+          fprintf(stdout, "Insufficient parameters passed.\n");
+          exit(1);
      }
 
-     if (output_file == NULL) {
-          printf("Output file does not exist\n");
-          return 0;
+     /* Output error if file source can not be read from or does not exist */
+     if ((filedir_src = open(argv[1], O_RDONLY)) == -1) {
+          perror(argv[1]);
+          exit(2);
      }
 
-     char ch;
-     while ((ch = fgetc(input_file)) != EOF) {
-          fputc(ch, output_file);
+     /* Output error if file destination is not created or replaced */
+     if ((filedir_des = creat(argv[2], 0644)) == -1) {
+          perror(argv[2]);
+          exit(3);
      }
 
-     fclose(input_file);
-     fclose(output_file);
+     /* Loop through source file directory, copy and write to destination file directory */
+     while ((line = read(filedir_src, buf, BUFSIZ)) > 0) {
+          if (write(filedir_des, buf, line) != line) {
+               perror(argv[2]);
+               exit(4);
+          } else { 
+               fprintf(stdout, "The contents of file %s have been successfully copied into the %s file.\n", argv[1], argv[2]);
+          }
+     }
+
+     /* Close both file directories */
+     close(filedir_src);
+     close(filedir_des);
 
      return 0;
 }
-
