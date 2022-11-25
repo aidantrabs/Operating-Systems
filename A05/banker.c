5908d03d9cf4526c96ed33c2283ca929b32e1a75
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-11-25 01:06:10 -0500

    Add banker file and add empty required commands

---
+++ A05/banker.c
@@ -0,0 +1,51 @@
+/**
+--------------------------------------
+Authors: Aidan Traboulay & Aleksander Neceski
+IDs: 200115590 & 201851860
+Emails: trab5590@mylaurier.ca & nece1860@mylaurier.ca
+--------------------------------------
+**/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+void request_resources();
+void release_resources();
+void status();
+void run();
+void exit();
+
+int main(int argc, char** argv) {
+
+}
+
+
+/* RQ Command */
+void request_resources() {
+
+}
+
+/* RL Command */
+void release_resources() {
+
+}
+
+/* Status Command */
+void status() {
+
+}
+
+/* Run Command */
+void run() {
+
+}
+
+/* Exit Command */
+void exit() {
+
+}
