a1bd15fb0e9f57d36f2cd0ce1262c07a1565b15c
AleksandarNe11 <aleks.neceski@gmail.com>
2022-12-04 22:51:50 -0500

    Affect allocated and available

--- A05/banker.c
+++ A05/banker.c
@@ -139,7 +139,21 @@ void release_resources(char* buf) {
     printf("thread_num: %d \n", thread_num);
     printf("To release: \n");
     for (int j = 0; j < num_resources; j++) { 
+        available[j] += temp[j];
+        allocated[thread_num][j] += temp[j];
         printf("%d ", temp[j]);
+    }
+    printf("\n");
+
+    printf("Now Available Resources: \n");
+    for (int j = 0; j < num_resources; j++) { 
+        printf("%d ", available[j]);
+    }
+    printf("\n");
+
+    printf("Resources still held by thread: \n");
+    for (int j = 0; j < num_resources; j++) { 
+        printf("%d ", allocated[thread_num][j]);
     }
     printf("\n");
 }
